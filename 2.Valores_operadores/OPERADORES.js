f1bd459c9d064c0eb54b0c087fe3de50f230e546
jhonatan-flores <henrryflorescupe@gmail.com>
2024-09-25 13:13:03 -0500

    clases de operadores aritmeticas

---
+++ 2.Valores_operadores/OPERADORES.js
@@ -0,0 +1,22 @@
+//Operadores Aritmetico
+//suma
+console.log("suma:",4+5)
+//resta
+console.log("resta:",9-5)
+//multiplicacion
+console.log("multiplicacion:",8*2)
+//divicion
+console.log("divicion:",10/2)
+//modulo
+console.log("modulo",10%3)
+//potencia
+console.log("potencia",2**3)
+
+/* realiza la operacion de sumar 4 y 100 y luego multiplicar el resultado por 11 */
+//estamos haciendo uso de la agrupacion de operacion.
+//procedencia de opeeradores
+/*establece el orden en el que de deve de ejecutar cada operador*/
+// para evitar recordar la presedencia se recomienda hacer uso 
+// de la a
+
+console.warn("resultado",(100+4)*11)
---
+++ 2.Valores_operadores/ejercicios.js
@@ -0,0 +1,18 @@
+//convertir expreciones aritmeticas
+//exprecion algoritmica
+//exprecion linial
+console.log((2+3)/(5*2))
+console.log(((10+3)/6)+4)
+console.log(5+(10)/(3+2))
+console.log(5+(10/2)+7)
+
+//ejercicio => a=4 B=2 C=6 d=8
+
+console.log(((4/2)+(2/6))/((4/2)-(2/6)))
+console.log(4+((4+(4+2)/(6+8))/4+(4+2)))
+console.log(4+2+(6/8)+(4/(2-6)/(4/(2+6))))
+console.log((4/(4+2))/(4/(4-2)))
+console.log((4+(2)/(4+2+(2/6)))/4+(2)/(6+4))
+console.log((4+2+6)/(4+(2/6)))
+console.log(((4+2)+(6)/(8*4))/((4+2)*(6/8)))
+console.log((4+(2/6)+8)/4)
