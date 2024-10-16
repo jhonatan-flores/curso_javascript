8da46aac0da6a48a2882d01d689e3a51fc728824
Jhonatan <132001096+jhonatan-flores@users.noreply.github.com>
2024-10-15 19:03:07 -0500

    Actualizar PracticaUno
    Feet: agrege los ejercicios 1 y 2

---
+++ 3.Estructura_del_Programa/PracticaUno/script.js
@@ -0,0 +1,9 @@
+// Ejercicio 1
+let num1 = prompt("Ingresa el primer número:");
+let num2 = prompt("Ingresa el segundo número:");
+let producto = num1 * num2;
+producto > 100 ? alert("El producto es mayor que 100") : alert("El producto es menor o igual a 100");
+
+// Ejercicio 2
+let password = prompt("Ingresa tu contraseña:");
+password === "miSecreta123" ? alert("Contraseña correcta") : alert("Contraseña incorrecta");
